a757eda4ed2621d31f4436a7d50c20fbbef7bbf1
Theo <kkw.theo@gmail.com>
2023-08-11 17:32:49 +0900

    target blank

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <p class="text-5xl tracking-[3px] font-bold text-center">SOON</p>
     <a
       class="tracking-[-1px] underline"
+      target="_blank"
       href="https://kkwon-theo.notion.site/2e211b9df81f473ca887d06afccaef1b?pvs=4"
       >이력서 보러가기</a
     >
