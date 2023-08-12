33822cb92a98fd4410861d99011860851bf360d8
Theo <kkw.theo@gmail.com>
2023-08-12 21:11:54 +0900

    header modify

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,20 @@
 <template>
   <div class="flex h-full flex-col">
     <header
-      class="flex w-full h-20 items-center px-2 justify-between dark:bg-[#29292c] fixed top-0 left-0 tablet:h-16 border-b dark:border-none"
+      class="grid grid-cols-2 w-full h-20 items-center px-2 justify-between dark:bg-[#29292c] fixed top-0 left-0 tablet:h-16 border-b dark:border-none"
     >
       <h1 class="text-xl -tracking-[0.8px] tablet:text-lg">
         <NuxtLink to="/"> Theo's Web Portfolio </NuxtLink>
       </h1>
-      <!-- <select
-        v-model="colorMode.value"
-        class="border w-20 h-8 dark:bg-[#1e1f21] dark:text-white dark:border-gray-700 outline-none text-center rounded text-sm"
-      >
-        <option v-for="theme in themeMode" :key="theme" :value="theme">
-          {{ theme }}
-        </option>
-      </select> -->
       <button
         type="button"
         @click="theme.themeModify"
-        class="dark:text-white dark:border-gray-700 outline-none text-center rounded text-sm -tracking-[0.5px] cursor-pointer"
+        class="dark:text-white dark:border-gray-700 outline-none rounded text-sm -tracking-[0.5px] cursor-pointer flex justify-end"
       >
         <div v-if="theme.currentTheme === 'dark'">
           <CommonSvgMoon width="70px" height="24px" />
         </div>
-        <div v-else-if="theme.currentTheme === 'light'">
+        <div v-else>
           <CommonSvgSun width="70px" height="24px" />
         </div>
       </button>
